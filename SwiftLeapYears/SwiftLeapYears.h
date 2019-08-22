7fdcd54bd7fe72f21b6e71d57bca26bd901949e4
Wasith Theerapattrathamrong <phai@live.com>
2019-08-22 15:12:46 +0700

    Init

---
+++ SwiftLeapYears/SwiftLeapYears.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLeapYears.h
+//  SwiftLeapYears
+//
+//  Created by Wasith Theerapattrathamrong on 22/8/2562 BE.
+//  Copyright © 2562 Wasith Theerapattrathamrong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftLeapYears.
+FOUNDATION_EXPORT double SwiftLeapYearsVersionNumber;
+
+//! Project version string for SwiftLeapYears.
+FOUNDATION_EXPORT const unsigned char SwiftLeapYearsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLeapYears/PublicHeader.h>
+
+
